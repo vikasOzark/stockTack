4f6ccb08876fb730c66800fcf1d85117a7d4da73
vikas <vikask99588@gmail.com>
2022-06-20 07:54:53 +0530

    error': "today's Date not supported yet

--- stocks/models.py
+++ stocks/models.py
@@ -7,13 +7,15 @@ class MyPortfolio(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     stock_name = models.CharField(max_length=20)
     stock_quantity = models.PositiveIntegerField()
-    is_favorite = models.BooleanField(default=False)
-    print('====> ')
+    is_favorite = models.BooleanField(default=False)    
     
     @property
     def stock_price(self):
-        stock_price = get_stock_search(self.stock_name)
-        # price = 
+        try:
+            stock_price = get_stock_search(self.stock_name)
+            print(stock_price)
+            stock_price = stock_price['close']
 
-        # print('====> ', price)
-        return self.stock_quantity * stock_price
+            return self.stock_quantity * stock_price
+        except:
+            return 'NOT_FOUND'
